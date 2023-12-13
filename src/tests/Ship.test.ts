8cbefab4942f32cb534c14a752db494c3fdc1eb1
News Guy <joewilliamdd31@gmail.com>
2023-12-13 22:25:05 +0100

    Correct the test in ship.test.ts

--- src/tests/Ship.test.ts
+++ src/tests/Ship.test.ts
@@ -4,7 +4,7 @@ describe("Ship Class", () => {
     it("creates an object with readonly 'shipsLength' as the only pub prop", () => {
         const a = new Ship(3);
 
-        expect(a.shipsLength).toBe(0);
+        expect(a.shipLength).toBe(3);
         expect(Object.keys(a)).toHaveLength(1);
     });
 });
