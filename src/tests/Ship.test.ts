c59d390eed4ff1745ea9a2eeb3e4fea2aaaa7e0e
News Guy <joewilliamdd31@gmail.com>
2023-12-13 22:21:02 +0100

    Import Ship class in ship.test.ts

--- src/tests/Ship.test.ts
+++ src/tests/Ship.test.ts
@@ -1,3 +1,5 @@
+import { Ship } from "../scripts/Ship";
+
 describe("Ship Class", () => {
     it("creates an object with readonly 'shipsLength' as the only pub prop", () => {
         const a = new Ship(3);
