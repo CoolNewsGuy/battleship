fe4e933f654d367ad6c328dcb0e464d8e5dd44f3
News Guy <joewilliamdd31@gmail.com>
2023-12-18 21:40:52 +0100

    Create 'grid' getter

--- src/scripts/Gameboard.ts
+++ src/scripts/Gameboard.ts
@@ -2,4 +2,8 @@ import { type Spot, type MatrixOf10x10 } from "../types";
 
 export class Gameboard {
     #grid: MatrixOf10x10<Spot>;
+
+    get grid(): MatrixOf10x10<Spot> {
+        return this.#grid;
+    }
 }
