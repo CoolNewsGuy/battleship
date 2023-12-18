469826b691310d51c01f3b73a030be82977f8476
News Guy <joewilliamdd31@gmail.com>
2023-12-18 21:27:16 +0100

    Test if Gameboard Class has no pub props

--- src/tests/Gameboard.test.ts
+++ src/tests/Gameboard.test.ts
@@ -1,3 +1,9 @@
+import { Gameboard } from "../scripts/Gameboard";
+
 describe("Gameboard Class", () => {
-    
+    it("has no pub props", () => {
+        const board = new Gameboard();
+
+        expect(Object.keys(board)).toHaveLength(0);
+    });
 });
