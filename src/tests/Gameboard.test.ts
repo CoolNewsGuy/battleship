8edd959a848e0653afa651598950c6b0bd100bcf
News Guy <joewilliamdd31@gmail.com>
2023-12-18 21:30:41 +0100

    Test if Gameboard Class has a getter for the grid

--- src/tests/Gameboard.test.ts
+++ src/tests/Gameboard.test.ts
@@ -6,4 +6,14 @@ describe("Gameboard Class", () => {
 
         expect(Object.keys(board)).toHaveLength(0);
     });
+
+    it("has a getter for a private prop 'grid' of 10x10 matrix value", () => {
+        const board = new Gameboard();
+
+        expect(board.grid()).toHaveLength(10);
+
+        for (const row of board.grid()) {
+            expect(row).toHaveLength(10);
+        }
+    });
 });
