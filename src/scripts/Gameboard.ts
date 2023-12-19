2f27ff7994ffc37ff30fb165d2e2c2bbeb68aaa3
News Guy <joewilliamdd31@gmail.com>
2023-12-19 20:00:52 +0100

    Initialize 'grid' Gameboard's property's value

--- src/scripts/Gameboard.ts
+++ src/scripts/Gameboard.ts
@@ -1,7 +1,15 @@
-import { type Spot, type MatrixOf10x10 } from "../types";
+import { Spot, type MatrixOf10x10 } from "../types";
 
 export class Gameboard {
     #grid: MatrixOf10x10<Spot>;
+
+    constructor() {
+        this.#grid = new Array<null>(10)
+            .fill(null)
+            .map(() =>
+                new Array<Spot>(10).fill(Spot.Empty)
+            ) as MatrixOf10x10<Spot>;
+    }
 
     get grid(): MatrixOf10x10<Spot> {
         return this.#grid;
