a8b0b966ff4749a5b0dd268086139f7477444dc5
News Guy <joewilliamdd31@gmail.com>
2023-12-21 16:47:03 +0100

    Make placeShip accepts a parameter for the ship

--- src/scripts/Gameboard.ts
+++ src/scripts/Gameboard.ts
@@ -1,5 +1,6 @@
 import { WrongCoordsError } from "../errors";
 import { Spot, type MatrixOf10x10 } from "../types";
+import { Ship } from "./Ship";
 
 export class Gameboard {
     readonly #grid: MatrixOf10x10<Spot>;
@@ -16,7 +17,11 @@ export class Gameboard {
         return this.#grid;
     }
 
-    placeShip(row: number, col: number): undefined | WrongCoordsError {
+    placeShip(
+        ship: Ship,
+        row: number,
+        col: number
+    ): undefined | WrongCoordsError {
         if (row < 0 || row > 9 || col < 0 || col > 9) {
             return new WrongCoordsError(row, col);
         }
