f1ee077d206a875998668fce8c0ee3edfe28d43e
News Guy <joewilliamdd31@gmail.com>
2023-12-21 17:18:18 +0100

    Create PlacingOptions interface

--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,5 @@
+import { type Ship } from "./scripts/Ship";
+
 export const enum Spot {
     Empty,
     Taken,
@@ -5,3 +7,9 @@ export const enum Spot {
 }
 export type ArrayOfLen10<T> = [T, T, T, T, T, T, T, T, T, T];
 export type MatrixOf10x10<T> = ArrayOfLen10<ArrayOfLen10<T>>;
+
+export interface PlacingOptions {
+    ship: Ship;
+    row: number;
+    col: number;
+}
