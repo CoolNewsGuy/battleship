aab6fea6c6361b59879f2f40f5233da20432450f
News Guy <joewilliamdd31@gmail.com>
2023-12-22 14:33:03 +0100

    Create NotEnoughSpotsError class

--- src/errors.ts
+++ src/errors.ts
@@ -1,3 +1,5 @@
+import { type PlacingOptions } from "./types";
+
 export class WrongCoordsError extends Error {
     readonly name: string;
     readonly message: string;
@@ -12,5 +14,23 @@ col: ${col}
 
 Both values must be > 0 and < 9.
 `;
+    }
+}
+
+export class NotEnoughSpotsError extends Error {
+    readonly name: string;
+    readonly message: string;
+
+    constructor(options: PlacingOptions) {
+        const leftNeededSpots = options.col + options.ship.shipLength - 10;
+
+        super();
+        this.name = "NotEnoughSpotsError";
+        this.message = `\
+Number of ${options.dir} spots is not enough to place the ship (Needs ${leftNeededSpots} more spots):
+row: ${options.row}
+col: ${options.col}
+ship's length: ${options.ship.shipLength}
+        `;
     }
 }
