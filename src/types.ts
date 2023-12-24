0dab4ffe5691daee012a5647c54e427f8b02f464
News Guy <joewilliamdd31@gmail.com>
2023-12-24 14:51:12 +0100

    Add 'Damaged' mem to Spot enum

--- src/types.ts
+++ src/types.ts
@@ -4,6 +4,7 @@ export const enum Spot {
     Empty,
     Taken,
     Missed,
+    Damaged,
 }
 export type ArrayOfLen10<T> = [T, T, T, T, T, T, T, T, T, T];
 export type MatrixOf10x10<T> = ArrayOfLen10<ArrayOfLen10<T>>;
