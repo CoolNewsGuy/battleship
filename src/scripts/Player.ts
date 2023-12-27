a6f7610d84b52d4894dec6e5e89afc6716edc729
News Guy <joewilliamdd31@gmail.com>
2023-12-27 10:24:51 +0100

    Pass Player's class 1st test

--- src/scripts/Player.ts
+++ src/scripts/Player.ts
@@ -1,3 +1,11 @@
+import { Gameboard } from "./Gameboard";
+
 export class Player {
-    
+    readonly name: string;
+    readonly board: Gameboard;
+
+    constructor(name: string) {
+        this.name = name;
+        this.board = new Gameboard();
+    }
 }
