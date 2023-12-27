af182170a7971995d865ecddb2f464890aa9507a
News Guy <joewilliamdd31@gmail.com>
2023-12-27 10:22:38 +0100

    Test: Player class has two readonly pub props

--- src/tests/Player.test.ts
+++ src/tests/Player.test.ts
@@ -1,3 +1,11 @@
+import { Gameboard } from "../scripts/Gameboard";
+import { Player } from "../scripts/Player";
+
 describe("Player class", () => {
-    
+    it("has readonly 'name' and 'board' pub props", () => {
+        const p = new Player("John");
+
+        expect(p.name).toBe("John");
+        expect(p.board).toStrictEqual(new Gameboard());
+    });
 });
