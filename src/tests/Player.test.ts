0f8de491102ed36e438a161a7dbda2c424adc880
News Guy <joewilliamdd31@gmail.com>
2023-12-28 09:37:26 +0100

    Test: attack method returns an error if the target is the attacker themselves

--- src/tests/Player.test.ts
+++ src/tests/Player.test.ts
@@ -1,6 +1,7 @@
 import { WrongCoordsError } from "../errors";
 import { Gameboard } from "../scripts/Gameboard";
 import { Player } from "../scripts/Player";
+import { Spot } from "../types";
 
 describe("Player class", () => {
     it("has readonly 'name' and 'board' pub props", () => {
@@ -34,6 +35,20 @@ describe("Player class", () => {
             [p1, p2].forEach((p) => {
                 expect(p.board).toStrictEqual(new Gameboard());
             });
+        });
+
+        it("returns an error if the target is the attacker themeselves", () => {
+            const p1 = new Player("foo");
+
+            expect(
+                p1.attack({
+                    player: p1,
+                    row: 3,
+                    col: 5,
+                })
+            ).toBeInstanceOf(AttackReceiverError);
+
+            expect(p1.board.grid[3][5]).toStrictEqual(Spot.Empty);
         });
     });
 });
