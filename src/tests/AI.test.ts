2b1ea3e1a09a03f7907500a6180129317f082367
News Guy <joewilliamdd31@gmail.com>
2023-12-31 18:42:00 +0100

    Test: AI class has its 'name' prop === 'AI' by default

--- src/tests/AI.test.ts
+++ src/tests/AI.test.ts
@@ -1,3 +1,9 @@
+import { AI } from "../scripts/AI";
+
 describe("AI Class", () => {
-    
+    it("has its 'name' prop === 'AI' by default", () => {
+        const ai = new AI();
+
+        expect(ai.name).toBe("AI");
+    });
 });
