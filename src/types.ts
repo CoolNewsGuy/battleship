b2980f4d0cf001d915af73573b1cfa5921aa792c
News Guy <joewilliamdd31@gmail.com>
2024-01-01 17:28:39 +0100

    Create NumFrom0to9 type

--- src/types.ts
+++ src/types.ts
@@ -9,6 +9,7 @@ export const enum Spot {
 }
 export type ArrayOfLen10<T> = [T, T, T, T, T, T, T, T, T, T];
 export type MatrixOf10x10<T> = ArrayOfLen10<ArrayOfLen10<T>>;
+export type NumFrom0To9 = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9;
 
 export interface SpotWithShip {
     spotStatus: Spot.Taken | Spot.Damaged;
