ab3a9f04332365be8dc33446c42cc1b25cab8112
News Guy <joewilliamdd31@gmail.com>
2024-01-01 17:10:11 +0100

    Declare overrided attack() in AI class

--- src/scripts/AI.ts
+++ src/scripts/AI.ts
@@ -1,7 +1,21 @@
+import {
+    type WrongCoordsError,
+    type AttackReceiverError,
+    type AlreadyAttackedSpotError,
+} from "../errors";
+import { type AttackOptions } from "../types";
 import { Player } from "./Player";
 
 export class AI extends Player {
     constructor() {
         super("AI");
     }
+
+    override attack(
+        options: AttackOptions
+    ):
+        | WrongCoordsError
+        | AttackReceiverError
+        | AlreadyAttackedSpotError
+        | undefined {}
 }
