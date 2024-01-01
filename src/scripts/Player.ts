324a42dd4d9376faae5f4d32b79808d401425f80
News Guy <joewilliamdd31@gmail.com>
2024-01-01 17:55:22 +0100

    Edit attack method

--- src/scripts/Player.ts
+++ src/scripts/Player.ts
@@ -1,8 +1,4 @@
-import {
-    AttackReceiverError,
-    type AlreadyAttackedSpotError,
-    WrongCoordsError,
-} from "../errors";
+import { AttackReceiverError, type AlreadyAttackedSpotError } from "../errors";
 import { type AttackOptions } from "../types";
 import { Gameboard } from "./Gameboard";
 
@@ -17,19 +13,11 @@ export class Player {
 
     attack(
         options: AttackOptions
-    ):
-        | undefined
-        | WrongCoordsError
-        | AttackReceiverError
-        | AlreadyAttackedSpotError {
-        const { row, col, receiver } = options;
+    ): undefined | AttackReceiverError | AlreadyAttackedSpotError {
+        const { receiver } = options;
 
         if (receiver === this) {
             return new AttackReceiverError();
-        }
-
-        if (row < 0 || row > 9 || col < 0 || col > 9) {
-            return new WrongCoordsError(row, col);
         }
 
         return receiver.board.receiveAttack(options);
