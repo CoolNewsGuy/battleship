66c885928cbfe3ef003cf6966145aee4dd2e6b48
News Guy <joewilliamdd31@gmail.com>
2024-01-07 10:01:58 +0100

    Pass overrided attack()'s 1st test

--- src/scripts/AI.ts
+++ src/scripts/AI.ts
@@ -1,4 +1,4 @@
-import { type AttackReceiverError } from "../errors";
+import { AttackReceiverError } from "../errors";
 import { type AttackOptions } from "../types";
 import { Player } from "./Player";
 
@@ -10,6 +10,10 @@ export class AI extends Player {
     override attack(
         options: Pick<AttackOptions, "receiver">
     ): AttackReceiverError | undefined {
-        
+        const { receiver } = options;
+
+        if (receiver === this) {
+            return new AttackReceiverError();
+        }
     }
 }
