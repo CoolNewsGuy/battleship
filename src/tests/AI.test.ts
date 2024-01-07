f7dcf9ca56acfbe9a05d22d76d70bb2caa954b73
News Guy <joewilliamdd31@gmail.com>
2024-01-07 10:30:43 +0100

    Test: overrided attack method attacks a random spot

--- src/tests/AI.test.ts
+++ src/tests/AI.test.ts
@@ -2,6 +2,8 @@ import { AttackReceiverError, GameoverError } from "../errors";
 import { AI } from "../scripts/AI";
 import { Gameboard } from "../scripts/Gameboard";
 import { Player } from "../scripts/Player";
+import { Ship } from "../scripts/Ship";
+import { Spot, type SpotWithShip } from "../types";
 
 describe("AI Class", () => {
     it("has its 'name' prop === 'AI' by default", () => {
@@ -36,6 +38,33 @@ describe("AI Class", () => {
             ).toBeInstanceOf(GameoverError);
 
             expect(p.board.grid).toStrictEqual(new Gameboard().grid);
+        });
+
+        it("attacks a random spot", () => {
+            const ai = new AI();
+            const p = new Player("foo");
+            const ship = new Ship(2);
+
+            p.board.placeShip({
+                ship,
+                row: 3,
+                col: 2,
+                dir: "horizontal",
+            });
+
+            for (let _ = 0; _ < 100; _++) {
+                ai.attack({ receiver: p });
+            }
+
+            expect(p.board.grid[3][2]).toStrictEqual<SpotWithShip>({
+                ship,
+                spotStatus: Spot.Damaged,
+            });
+
+            expect(p.board.grid[3][3]).toStrictEqual<SpotWithShip>({
+                ship,
+                spotStatus: Spot.Damaged,
+            });
         });
     });
 });
