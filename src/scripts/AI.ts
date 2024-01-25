15a26f5942510403c4966b83576e94ac936f23a5
News Guy <joewilliamdd31@gmail.com>
2024-01-25 21:33:10 +0100

    Pass overrided attack()'s 3rd test

--- src/scripts/AI.ts
+++ src/scripts/AI.ts
@@ -1,5 +1,10 @@
 import { AttackReceiverError, GameoverError } from "../errors";
-import { type AttackOptions } from "../types";
+import {
+    type NumFrom0To9,
+    type AttackOptions,
+    Spot,
+    type SpotWithShip,
+} from "../types";
 import { Player } from "./Player";
 
 export class AI extends Player {
@@ -19,5 +24,20 @@ export class AI extends Player {
         if (receiver.board.areAllShipsSunk()) {
             return new GameoverError();
         }
+
+        let randomRow: NumFrom0To9;
+        let randomCol: NumFrom0To9;
+        let target: Spot | SpotWithShip;
+
+        do {
+            randomRow = Math.floor(Math.random() * 10) as NumFrom0To9;
+            randomCol = Math.floor(Math.random() * 10) as NumFrom0To9;
+            target = receiver.board.grid[randomRow][randomCol];
+        } while (
+            target === Spot.Missed ||
+            (target instanceof Object && target.spotStatus === Spot.Damaged)
+        );
+
+        receiver.board.receiveAttack({ row: randomRow, col: randomCol });
     }
 }
