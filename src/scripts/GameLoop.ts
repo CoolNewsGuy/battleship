4754921c7b04136c00a30133fcfdefadd7aeb506
The Cool News Guy <joewilliamdd31@gmail.com>
2024-01-31 18:20:33 +0100

    Pass GameLoop's 1st test

--- src/scripts/GameLoop.ts
+++ src/scripts/GameLoop.ts
@@ -1,3 +1,12 @@
+import { type AI } from "./AI";
+import { type Player } from "./Player";
+
 export class GameLoop {
-    
+    readonly #playerOne: Player | AI;
+    readonly #playerTwo: Player | AI;
+
+    constructor(playerOne: Player | AI, playerTwo: Player | AI) {
+        this.#playerOne = playerOne;
+        this.#playerTwo = playerTwo;
+    }
 }
