8fb259cbfdcc534a2aeb2088d50a6e1162ba508c
The Cool News Guy <joewilliamdd31@gmail.com>
2024-01-31 18:17:36 +0100

    Test: GameLoop has no pub props

--- src/tests/GameLoop.test.ts
+++ src/tests/GameLoop.test.ts
@@ -1,3 +1,10 @@
+import { AI } from "../scripts/AI";
+import { Player } from "../scripts/Player";
+
 describe("GameLoop class", () => {
-    
-})
+    it("has no pub props", () => {
+        const game = new GameLoop(new Player("foo"), new AI("bar"));
+
+        expect(Object.keys(game)).toHaveLength(0);
+    });
+});
