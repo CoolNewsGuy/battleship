36e498e32fc9f0002033a8eefa8e1f80932d7bf2
The Cool News Guy <joewilliamdd31@gmail.com>
2024-02-16 16:31:15 +0100

    Create a basic renderTo method in GameboardView class

--- src/scripts/Gameboard/GameboardView.ts
+++ src/scripts/Gameboard/GameboardView.ts
@@ -1,4 +1,6 @@
-import { HTMLClass } from "../../DOMTypes";
+import { HTMLClass, type RowLetter } from "../../DOMTypes";
+import { type MatrixOf10x10, type Spot, type SpotWithShip } from "../../types";
+import { type Ship } from "../Ship";
 
 export class GameboardView {
     private readonly gameboardElement: HTMLDivElement;
@@ -6,9 +8,120 @@ export class GameboardView {
     constructor() {
         this.gameboardElement = document.createElement("div");
         this.gameboardElement.className = HTMLClass.Gameboard;
-
-        this.renderTo(document.body);
     }
 
-    private renderTo(element: HTMLElement): void {}
+    renderTo(
+        element: HTMLElement,
+        grid: MatrixOf10x10<Spot | SpotWithShip>,
+        placedShips: readonly Ship[]
+    ): void {
+        const lostShipsContainerDiv = document.createElement("div");
+        const lostShipsLengthsDiv = document.createElement("div");
+
+        lostShipsContainerDiv.className = HTMLClass.LostShipsContainer;
+        lostShipsLengthsDiv.className = HTMLClass.LostShipsLengths;
+        lostShipsLengthsDiv.textContent = `(${placedShips
+            .filter((ship) => ship.isSunk())
+            .join(", ")})`;
+
+        lostShipsContainerDiv.innerHTML = `
+        <p>
+            Lost Ships' Lengths:
+        ${lostShipsLengthsDiv.outerHTML}
+        </p>
+        `;
+
+        const shipsContainerDiv = document.createElement("div");
+
+        for (const ship of placedShips) {
+            const shipDiv = document.createElement("div");
+
+            shipDiv.classList.add(HTMLClass.Ship, HTMLClass.NormalShip);
+            shipsContainerDiv.appendChild(shipDiv);
+
+            for (let _ = 0; _ < ship.shipLength; _++) {
+                const squareDiv = document.createElement("div");
+
+                squareDiv.className = HTMLClass.Square;
+                shipDiv.appendChild(squareDiv);
+            }
+        }
+
+        const gridDiv = document.createElement("div");
+
+        gridDiv.className = HTMLClass.Grid;
+
+        for (let i = 0; i < 11; i++) {
+            const rowDiv = document.createElement("div");
+
+            rowDiv.className = HTMLClass.Row;
+
+            // Columns' Numbers row
+            if (i === 0) {
+                rowDiv.classList.add(HTMLClass.ColumnsNumbers);
+
+                for (let j = 0; j < 11; j++) {
+                    const squareDiv = document.createElement("div");
+
+                    squareDiv.className = HTMLClass.Square;
+
+                    if (j === 0) {
+                        squareDiv.classList.add(HTMLClass.EmptySquare);
+                    } else {
+                        squareDiv.classList.add(HTMLClass.ColumnNumber);
+                        squareDiv.textContent = `${j}`;
+                    }
+
+                    rowDiv.appendChild(squareDiv);
+                }
+            } else {
+                const rowLetters: RowLetter[] = [
+                    "A",
+                    "B",
+                    "C",
+                    "D",
+                    "E",
+                    "F",
+                    "G",
+                    "H",
+                    "I",
+                    "J",
+                ];
+
+                for (let j = 0; j < 11; j++) {
+                    const squareDiv = document.createElement("div");
+
+                    squareDiv.className = HTMLClass.Square;
+
+                    if (j === 0) {
+                        squareDiv.classList.add(HTMLClass.RowLetter);
+                        squareDiv.textContent = rowLetters[i - 1];
+                    } else {
+                        squareDiv.classList.add(HTMLClass.PlayingSquare);
+                    }
+
+                    rowDiv.appendChild(squareDiv);
+                }
+            }
+
+            gridDiv.appendChild(rowDiv);
+        }
+
+        const PlayerNameContainerDiv = document.createElement("div");
+        const PlayerNameSpan = document.createElement("span");
+
+        PlayerNameContainerDiv.className = HTMLClass.PlayerNameContainer;
+        PlayerNameSpan.className = HTMLClass.PlayerName;
+
+        PlayerNameContainerDiv.appendChild(PlayerNameSpan);
+
+        this.gameboardElement.append(
+            lostShipsContainerDiv,
+            shipsContainerDiv,
+            gridDiv,
+            PlayerNameContainerDiv
+        );
+
+        element.appendChild(this.gameboardElement);
+    }
 }
