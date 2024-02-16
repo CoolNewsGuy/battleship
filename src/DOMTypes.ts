f0f99019a1a62e58430b191385371312e0e2cbe8
The Cool News Guy <joewilliamdd31@gmail.com>
2024-02-16 04:19:38 +0100

    Create 'HTMLClass' enum

---
+++ src/DOMTypes.ts
@@ -0,0 +1,25 @@
+export const enum HTMLClass {
+    Gameboard = "gameboard",
+
+    LostShipsContainer = "lost-ships-container",
+    LostShipsLengths = "lost-ships-lengths",
+
+    ShipsContainer = "ships-container",
+    Ship = "ship",
+    NormalShip = "normal-ship",
+    SelectedShip = "selected-ship",
+    DamagedShip = "damaged-ship",
+    Square = "square",
+
+    Grid = "grid",
+    ColumnsNumbers = "columns-numbers",
+    Row = "row",
+    EmptySquare = "empty-square",
+    ColumnNumber = "column-number",
+    NormalRow = "normal-row",
+    RowLetter = "row-letter",
+    PlayingSquare = "playing-square",
+
+    PlayerNameContainer = "player-name-container",
+    PlayerName = "player-name",
+}
